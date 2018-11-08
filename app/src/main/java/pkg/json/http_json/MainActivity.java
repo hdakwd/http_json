a923e03b6d556e62c63bd5130040e3abbffb1cd2
hdakwd <egotism49@gmail.com>
2018-11-08 22:43:24 +0900

    htttp and json test.

--- app/src/main/java/pkg/json/http_json/MainActivity.java
+++ app/src/main/java/pkg/json/http_json/MainActivity.java
@@ -30,56 +30,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btClick = findViewById(R.id.buttonGet);
+        HTTP_BTN listener = new HTTP_BTN();
+        btClick.setOnClickListener(listener);
+
         //textViewUrl = (TextView)findViewById(R.id.textViewUrl);
-        textViewTime = (TextView)findViewById(R.id.textViewTime);
-        editTextUrl = (EditText)findViewById(R.id.editTextUrl);
-        buttonGet = (Button)findViewById(R.id.buttonGet);
-        textViewTime = (TextView)findViewById(R.id.textViewTime);
-        editTextResponse = (EditText)findViewById(R.id.editTextResponse);
+        textViewTime = (TextView) findViewById(R.id.textViewTime);
+        editTextUrl = (EditText) findViewById(R.id.editTextUrl);
+        buttonGet = (Button) findViewById(R.id.buttonGet);
+        textViewTime = (TextView) findViewById(R.id.textViewTime);
+        editTextResponse = (EditText) findViewById(R.id.editTextResponse);
 
         editTextUrl.setText("https://drive-rcd.tk/test.php");
     }
 
-    public void onButtonGetTest(View view) {
-        Toast.makeText(this,"click test",Toast.LENGTH_SHORT).show();
-    }
+    private class HTTP_BTN implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        URL url = new URL(editTextUrl.getText().toString());
+                        // 処理開始時刻
+                        startTime = System.currentTimeMillis();
+                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                        final String str = InputStreamToString(con.getInputStream());
+                        // 処理終了時刻
+                        endTime = System.currentTimeMillis();
+                        Log.d("HTTP", str);
 
-    public void onButtonGet(View view) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL(editTextUrl.getText().toString());
-                    // 処理開始時刻
-                    startTime = System.currentTimeMillis();
-                    HttpURLConnection con = (HttpURLConnection)url.openConnection();
-                    final String str = InputStreamToString(con.getInputStream());
-                    // 処理終了時刻
-                    endTime = System.currentTimeMillis();
-                    Log.d("HTTP", str);
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            editTextResponse.setText(String.valueOf(str));
-                            textViewTime.setText("処理時間：" + (endTime - startTime) + "ms");
-                        }
-                    });
-                } catch (Exception ex) {
-                    System.out.println(ex);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                editTextResponse.setText(String.valueOf(str));
+                                textViewTime.setText("処理時間：" + (endTime - startTime) + "ms");
+                            }
+                        });
+                    } catch (Exception ex) {
+                        System.out.println(ex);
+                    }
                 }
-            }
-        }).start();
-    }
-
-    static String InputStreamToString(InputStream is) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line);
+            }).start();
         }
-        br.close();
-        return sb.toString();
+
+        String InputStreamToString(InputStream is) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            br.close();
+            return sb.toString();
+        }
     }
 }
